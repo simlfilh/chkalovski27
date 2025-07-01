946033f3b7321d9d3d1fcbea14e2542e51232833
Valeria <148010378+simlfilh@users.noreply.github.com>
2025-07-01 21:13:05 +0300

    Update 1_info_location.py

--- chkalovski27/1_info_location.py
+++ chkalovski27/1_info_location.py
@@ -47,7 +47,7 @@ with col1:
                 background-color: #B3E5FC;
                 border-radius: 10px;
                 padding: 10px;
-                margin-bottom: 10px;
+                margin-bottom: 20px;
                 position: relative;
             }
             .text-indent-content {
@@ -60,9 +60,13 @@ with col1:
                 <div class="text-indent-content">
                     <h3 class="highlight-blue">Адрес: Санкт-Петербург, Чкаловский пр-т, д. 27</h3>
                 </div>
-            <br>  
+            <br>
+            <br>
+            <br>
             <p>→ от м. Петроградская 10 минут пешком</p>  
             <p>→ от м. Чкаловская 10 минут пешком</p>  
+            <br>
+            <br>
             <br>
         </div>
                 """, unsafe_allow_html=True)
@@ -75,8 +79,8 @@ with col2:
     st.components.v1.html("""
         <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d7987.415451973477!2d30.294856982375293!3d59.967747548874065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x4696315ae8aaae41%3A0x71c197f2c712c2a2!2z0JzQtdGC0YDQviDQn9C10YLRgNC-0LPRgNCw0LTRgdC60LDRjywg0KHQsNC90LrRgi3Qn9C10YLQtdGA0LHRg9GA0LM!3m2!1d59.966535!2d30.31101!4m5!1s0x4696314fcabaea25%3A0x4df5379af82456c8!2z0KfQutCw0LvQvtCy0YHQutC40Lkg0L_RgNC-0YHQv9C10LrRgiwgMjcsINCh0LDQvdC60YIt0J_QtdGC0LXRgNCx0YPRgNCz!3m2!1d59.965905!2d30.2992991!5e0!3m2!1sru!2sru!4v1751375950695!5m2!1sru!2sru" 
             width="600" 
-            height="450" 
-            style="border:0;" 
+            height="430" 
+            style="border:0; border-radius: 10px;" 
             allowfullscreen="" 
             loading="lazy" 
             referrerpolicy="no-referrer-when-downgrade">
